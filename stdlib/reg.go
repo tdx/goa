44d1368cebd7450d628e06f709b98a8a14c9ef2a
Andrew Gopienko <a.gopienko@ndmsystems.com>
2018-09-21 21:29:52 +0700

    Name lookup in default environment

--- stdlib/reg.go
+++ stdlib/reg.go
@@ -21,7 +21,14 @@ func (pid *Pid) Unregister(name Term) error {
 }
 
 //
-// Whereis lookups process by name
+// Whereis lookups process by name in default environment
+//
+func Whereis(name Term) (*Pid, error) {
+	return env.Whereis(name)
+}
+
+//
+// Whereis lookups process by name in specified environment
 //
 func (e *Env) Whereis(name Term) (*Pid, error) {
 	return e.whereis(name)
@@ -55,14 +62,28 @@ func (pid *Pid) UnregisterPrefix(prefix string, name Term) error {
 }
 
 //
-// WhereisPrefix lookups process by prefix and name
+// WhereisPrefix lookups process by prefix and name in default environment
+//
+func WhereisPrefix(prefix string, name Term) (*Pid, error) {
+	return env.WhereisPrefix(prefix, name)
+}
+
+//
+// WhereisPrefix lookups process by prefix and name in specified environment
 //
 func (e *Env) WhereisPrefix(prefix string, name Term) (*Pid, error) {
 	return e.whereisPrefix(prefix, name)
 }
 
 //
-// Whereare lookups processes by prefix
+// Whereare lookups processes by prefix in default environment
+//
+func Whereare(prefix string) (RegMap, error) {
+	return env.Whereare(prefix)
+}
+
+//
+// Whereare lookups processes by prefix in specified environment
 //
 func (e *Env) Whereare(prefix string) (RegMap, error) {
 	return e.whereare(prefix)
